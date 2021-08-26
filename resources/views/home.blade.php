837ed7ceccd71eb4a9df582a602d867138b2daf4
ConvertToInt <tdalgleish7@gmail.com>
2021-08-26 17:54:40 +0100

    home page and layout changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,8 +2,20 @@
 
 @section('head')
 
+<style>
+.aligncenter {
+    text-align: center;
+}
+</style>
+
 @endsection
 
 @section('content')
+
+<p class="aligncenter">
+    <img class="mt-6" src="logo.jpg" alt="centered image" style="width:40%"/>
+</p>
+
+{{-- <h1 class="title has-text-centered has-text-weight-bold has-text-grey-darker is-size-1 pt-3 mb-6">Understand autism from a unique perspective.</h1> --}}
 
 @endsection
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,32 +5,40 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.9.1/css/bulma.css" />
+    <script src="https://kit.fontawesome.com/09255c1d6c.js" crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Balsamiq+Sans:wght@700&display=swap" rel="stylesheet">
     <title>Atlas | Scottish Autism Training</title>
 
-    @yield('content')
+    @yield('head')
 
     <style>
         html{
             background-color:hsl(0, 0%, 92%);
         }
+
+        body{
+            font-family: 'Balsamiq Sans', cursive;
+        }
     </style>
 </head>
 <body>
-    <div class="columns navbar">
-        <div class="column nav button pt-5" style="border:2px solid black; background-color:#d53021; height:5em">
+    <div class="columns navbar mb-6">
+        <div class="column nav button pt-5" style="border:2px solid black; background-color:#d53021; height:4.5em; border-radius:0;">
             <a href="{{ route('home') }}"><h1 class="title">Home</h1></a>
         </div>
-        <div class="column nav button pt-5" href="{{ route('about') }}" style="border:2px solid black; background-color:#dc8432; height:5em">
+        <div class="column nav button pt-5" href="{{ route('about') }}" style="border:2px solid black; background-color:#dc8432; height:4.5em; border-radius:0;">
             <a href="{{ route('about') }}"><h1 class="title">About Us</h1></a>
         </div>
-        <div class="column nav button pt-5" href="{{ route('services') }}" style="border:2px solid black; background-color:#ffd94a; height:5em">
+        <div class="column nav button pt-5" href="{{ route('services') }}" style="border:2px solid black; background-color:#ffd94a; height:4.5em; border-radius:0;">
             <a href="{{ route('services') }}"><h1 class="title">Services</h1></a>
         </div>
-        <div class="column nav button pt-5" href="{{ route('contact') }}" style="border:2px solid black; background-color:#c0e749; height:5em">
+        <div class="column nav button pt-5" href="{{ route('contact') }}" style="border:2px solid black; background-color:#c0e749; height:4.5em; border-radius:0;">
             <a href="{{ route('contact') }}"><h1 class="title">Contact Us</h1></a>
         </div>
-        <div class="column nav button pt-5" href="" style="border:2px solid black; background-color:#8a31eb; height:5em">
-            <h1 class="title">[Socials]</h1>
+        <div class="column nav button pt-5" href="" style="border:2px solid black; background-color:#8a31eb; height:4.5em; border-radius:0;">
+            <h1 class="title"></h1>
         </div>
     </div>
 
