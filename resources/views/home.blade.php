165c4cf403f34620315a5a7cc2005a361c59bbb7
ConvertToInt <tdalgleish7@gmail.com>
2021-09-02 20:49:01 +0100

    Bulma install. bulma navbar. home page update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,12 +2,6 @@
 
 @section('head')
 
-<style>
-.aligncenter {
-    text-align: center;
-}
-</style>
-
 @endsection
 
 @section('content')
@@ -16,6 +10,90 @@
     <img class="mt-6" src="logo.jpg" alt="centered image" style="width:40%"/>
 </p> --}}
 
-{{-- <h1 class="title has-text-centered has-text-weight-bold has-text-grey-darker is-size-1 pt-3 mb-6">Understand autism from a unique perspective.</h1> --}}
+{{-- <h1 class="title has-text-centered has-text-weight-bold has-text-grey-darker pt-3 mb-6" style="font-size:100px">UNDERSTAND AUTISM FROM A UNIQUE PERSPECTIVE.</h1>  --}}
+
+<section class="hero is-large" style="background-color:#e0e0e0; border-bottom:8px dotted hsl(0, 100%, 42%)">
+    {{-- <span style="height: 37em; width: 0.70em; background-color: rgb(221, 0, 0); border-radius: 99px; margin: 5em 0 auto 1.75em; position:absolute;"></span> --}}
+    <div class="hero-body">
+        <p class="title has-text-centered" style="font-size:3.5em">
+            Understand Autism From a Unique Perspective.
+        </p><br>
+        <p class="title has-text-centered" style="font-size:2.5em">
+            How much do you know about autism?
+        </p>
+    </div>
+</section>
+
+<section class="hero is-halfheight" style="background-color:hsl(0, 0%, 92%);border-bottom:8px dotted hsl(34, 98%, 43%)">
+    {{-- <span style="height: 20em; width: 0.70em; background-color: rgb(221, 151, 0); border-radius: 99px; margin: 2em 0 auto 1.75em; position:absolute;"></span> --}}
+    <div class="hero-body">
+        <div class="columns">
+            <div class="column is-8">
+                <p class="title has-text-centered font-sm sizeable">
+                    Autism affects many people across communities around the country, however, many people on the spectrum 
+                    continue to feel misunderstood and that their needs are not being met. We want to change things by 
+                    sharing our own experiences and knowledge so that autistic people and their families feel safe, valued, and listened to.
+                </p>
+            </div>
+            <div class="column">
+                <i class="far fa-smile-beam" style="display:flex;align-items:center; justify-content:center; font-size:12em"></i>
+            </div>
+        </div>
+        
+    </div>
+</section>
+
+<section class="hero is-small" style="background-color:#e0e0e0; border-bottom:8px dotted hsl(54, 98%, 43%)" id="about">
+    <div class="hero-body">
+        <p class="title has-text-centered" style="font-size:4.5em">
+            Who are we?
+        </p>
+    </div>
+</section>
+
+<section class="hero is-medium" style="background-color:hsl(0, 0%, 92%); border-bottom:8px dotted rgb(0, 150, 0)">
+    <div class="hero-body">
+        <div class="columns">
+            <div class="column">
+                <p class="title has-text-centered font-sm">
+                    Autism ATLAS is an autism-led social enterprise, with a training team who want to improve the lives of autistic people through sharing 
+                    our experiences, alongside current research and guidance for good autism practice, to give people a more in-depth understanding of autism.
+                </p>
+            </div>
+            <div class="column pt-6 pb-6">
+                <i class="fas fa-book" style="display:flex;align-items:center; justify-content:center; font-size:12em;"></i>
+            </div>
+            <div class="column">
+                <p class="title has-text-centered font-sm">
+                    We all have different experiences, different educational backgrounds, different ways our autism affects us. But we all share one thing 
+                    in common: we want to share our stories with you, so that you might be able to make a genuine, lasting difference for 
+                    someone with autism.
+                </p>
+            </div>
+        </div>
+        
+        {{-- <p class="title has-text-centered font-sm">
+            We all have different diagnoses (whether it’s autism or Asperger’s Syndrome), different educational backgrounds, different diagnosis experiences, 
+            different ways our autism affects us,  different lives. But we all share one thing in common: we want to share our stories with you, so that you might be able to make a genuine, lasting difference for someone with autism in your life.
+        </p> --}}
+        
+    </div>
+</section>
+
+<section class="hero is-halfheight" style="background-color:#e0e0e0;">
+    <div class="hero-body">
+        <div class="columns">
+            <div class="column">
+                <i class="fas fa-hands-helping" style="display:flex;align-items:center; justify-content:center; font-size:12em;"></i>
+            </div>
+            <div class="column is-8">
+                <p class="title has-text-centered font-sm">
+                    However, it’s not just about telling people about autism (as much as we enjoy talking about it!) It's about collaborating with
+                     members of communities and working together to help people get the knowledge they need to help develop their good autism practice. 
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
 
 @endsection
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,12 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.9.1/css/bulma.css" />
-    <script src="https://kit.fontawesome.com/09255c1d6c.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/mystyles.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@700&display=swap" rel="stylesheet">
     <script src = "https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="https://kit.fontawesome.com/09255c1d6c.js" crossorigin="anonymous"></script>
     <title>Atlas | Scottish Autism Training</title>
 
     @yield('head')
@@ -17,51 +17,37 @@
     <style>
 
         *{
-            font-family: 'Ubuntu', cursive;
+            font-family: 'Ubuntu';
+            color:hsl(0, 0%, 28%) !important;
+            scroll-behavior: smooth !important;
         }
         html{
             background-color:hsl(0, 0%, 92%);
         }
-        .inverted{
-            color:green !important;
-            background-color: black !important;
-        }
-        .defaultScheme{
-            background: linear-gradient( -45deg, #c0e749, #c0e749 50%, #ffd94a 50%, #ffd94a 50%, #ffd94a 50%);
-        }
-        .invertedScheme{
-            background: linear-gradient( -45deg, green, green 50%, white 50%, white 50%, black 50% );
-        }
 
         .font-sm{
-            font-size:1.75em !important;
-            padding-top:7.5px
-        }
-
-        .font-md{
-            font-size:2em !important;
+            font-size:1.85em;
             padding-top:4px
         }
 
         .font-lg{
-            font-size:2.5em !important; 
+            font-size:2.25em; 
         }
-        .access-button{
-            width:35px;
-            height:35px;
-            border-radius:100%;
-            border:2px solid black;
-            display:inline-block;
+        .font-button{
+            display:flex;
+            width:55px;
+            height:55px;
+            border-radius:50%;
+            border:3px solid hsl(0, 0%, 40%);
+            align-items:center;
+            justify-content:center;
+            color:hsl(0, 0%, 30%);
         }
-        .navbar-link{
-            background-color:rgba(0,0,0,0) !important;
-        }
-        .nav{
-            background-color:hsl(0, 0%, 92%);
-            height:4.5em !important;
-            border-radius:0;
-            padding-top:1.25em;
-            border:0 !important;
+
+        /* hide font-button at 1230px and below */
+
+        .spaced{
+            margin:0 1rem 0 1rem;
         }
 
         .hoverable:hover{
@@ -70,15 +56,12 @@
         }
 
         footer{
-            margin-top:60px;
-            /* background-color:#e9c543; */
-            background-color:#cacaca;
+            background-color:#cfcfcf;
             text-align: center;
-            padding:12px 0 17px 0;
+            padding:20px 0 17px 0;
             font-weight:700;
             letter-spacing:3px;
-            border-top:2px solid black;
-            border-bottom:2px solid black
+            border-top:8px dotted rgb(77, 0, 150);
         }
 
         .socials{
@@ -107,25 +90,31 @@
         }
 
         .overline span, .underline span {
-        display: inline-block;
+            display: inline-block;
         }
 
         .overline span::before
         {
-        content: '';
-        height: 7px;
-        border-radius: 20px;
-        display: block;
-        margin-bottom:9px
+            content: '';
+            height: 7px;
+            border-radius: 20px;
+            display: block;
+            margin-bottom:9px
+        }
+
+        .underline span::after
+        {
+            content: '';
+            height: 6px;
+            border-radius: 20px;
+            display: block;
+            margin-top:9px;
+            opacity:0;
         }
 
         .underline span:hover:after
         {
-        content: '';
-        height: 6px;
-        border-radius: 20px;
-        display: block;
-        margin-top:9px;
+            opacity:1;
         }
 
         .red span::before, .red span::after{
@@ -147,8 +136,7 @@
         }
 
         ::-webkit-scrollbar {
-            width: 10px;
-            height: 6px;
+            width: 12px;
         }
         ::-webkit-scrollbar-thumb {
             border-radius: 10px;
@@ -164,51 +152,97 @@
     </style>
 
     <script>document.documentElement.className = document.documentElement.className.replace('no-js','js');</script>
+    
 </head>
 <body>
-    <div class="columns navbar mb-6" style="background-color:hsl(0, 0%, 92%);">
-        <div class="column nav is-2">
-            <img class="mt-0 pt-0 ml-5"src="logo.jpg" alt="centered image" style="width:150px; margin-bottom:-10px"/>
-        </div>
-        <div class="column nav button invertable" style="margin-top:2.5em">
-            <a href="{{ route('home') }}"><h1 class="title font-md invertable sizeable overline underline red"><span>Home</span></h1></a>
-        </div>
-        <div class="column nav button invertable" style="margin-top:2.5em">
-            <a href="{{ route('about') }}"><h1 class="title font-md invertable sizeable overline underline yellow"><span>About Us</span></h1></a>
-        </div>
-        <div class="column nav button invertable" style="margin-top:2.5em">
-            <a href="{{ route('contact') }}"><h1 class="title font-md invertable sizeable overline underline orange"><span>Our Team</span></h1></a>
-        </div>
-        <div class="column nav button invertable" style="margin-top:2.5em">
-            <a href="{{ route('services') }}"><h1 class="title font-md invertable sizeable overline underline green"><span>Services</span></h1></a>
-        </div>
-        <div class="column nav button invertable" style="margin-top:2.5em">
-            <a href="{{ route('contact') }}"><h1 class="title font-md invertable sizeable overline underline purple"><span>Contact Us</span></h1></a>
-        </div>
-        <div class="column nav is-1 pl-5 invertable" style="margin-top:3em">
-            <span class="navbar-item has-dropdown is-hoverable">
-                <i class="fas fa-cog is-size-2 navbar-link is-arrowless has-text-black-bis invertable"></i>
-                <div class="navbar-dropdown is-right is-boxed is-size-6">
-                    <a class="navbar-item font-sm">
-                        Aa
-                    </a>
-                    <a class="navbar-item has-text-weight-medium font-md">
-                        Aa
-                    </a>
-                    <a class="navbar-item has-text-weight-bold font-lg">
-                        Aa
-                    </a>
-                    <hr class="navbar-divider">
-                    <div class="navbar-item is-size-4">
-                        <div class="defaultScheme access-button hoverable"></div> &nbsp;&nbsp; <div class="invertedScheme hoverable access-button"></div>
-                    </div>
-                </div>
-            </span>
-        </div>
-    </div>
 
-    {{-- <div class="columns navbar mb-6" style="background-color:hsl(0, 0%, 92%);">
-        {{-- <div class="column nav is-1" style="background-color:#d53021;">
+    <nav class="navbar" role="navigation" aria-label="main navigation" style="border-bottom:1px solid hsl(0, 0%, 40%)">
+        <div class="navbar-brand">
+          <a class="navbar-item" href="{{route('home')}}">
+            <img src="logo.jpg" width="150" height="150">
+          </a>
+      
+          <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </a>
+        </div>
+      
+        <div class="navbar-menu">
+          <div class="navbar-start" style="flex-grow: 1; justify-content: center;">
+            <a class="navbar-item font-sm spaced invertable sizeable overline underline red" href="{{route('home')}}">
+              <span>Home</span>
+            </a>
+      
+            <a class="navbar-item font-sm spaced invertable sizeable overline underline orange" href="#about">
+              <span>About Us</span>
+            </a>
+
+            <a class="navbar-item font-sm spaced invertable sizeable overline underline yellow" href="{{route('team')}}">
+                <span>Our Team</span>
+            </a>
+      
+            <div class="navbar-item has-dropdown is-hoverable">
+              <a class="navbar-link is-arrowless font-sm spaced invertable sizeable overline underline green" href="{{route('services')}}">
+                <span>Services</span>
+              </a>
+      
+              {{-- <div class="navbar-dropdown is-boxed">
+                <a class="navbar-item font-sm">
+                  Service 1
+                </a>
+                <a class="navbar-item font-sm">
+                  Service 2
+                </a>
+                <a class="navbar-item font-sm">
+                  Service 3
+                </a>
+                <hr class="navbar-divider">
+                <a class="navbar-item font-sm">
+                  All Services
+                </a>
+              </div> --}}
+            </div>
+
+            <a class="navbar-item font-sm spaced invertable sizeable overline underline purple" href="{{route('contact')}}">
+                <span>Contact Us</span>
+            </a>
+          </div>
+      
+          <div class="navbar-end">
+            {{-- <div class="navbar-item">
+                <span class="navbar-item has-dropdown is-hoverable">
+                    <i class="fas fa-cog is-size-1 navbar-link is-arrowless has-text-black-bis invertable"></i>
+                    <div class="navbar-dropdown is-right is-boxed is-size-6">
+                        <a class="navbar-item font-sm">
+                            Aa
+                        </a>
+                        <a class="navbar-item has-text-weight-medium font-md">
+                            Aa
+                        </a>
+                        <a class="navbar-item has-text-weight-bold font-lg">
+                            Aa
+                        </a>
+                        <hr class="navbar-divider">
+                        <div class="navbar-item is-size-4">
+                            <div class="defaultScheme access-button hoverable"></div> &nbsp;&nbsp; <div class="invertedScheme hoverable access-button"></div>
+                        </div>
+                    </div>
+                </span>
+            </div> --}}
+            <a class="navbar-item font-sm p-1 has-text-centered" style="font-size:20px !important">
+                <div class="hoverable font-button"><i class="fas fa-font"></i></div>
+            </a>
+            <a class="navbar-item has-text-weight-medium font-lg mr-3 p-1">
+                <div class="hoverable font-button"><i class="fas fa-font"></i></div>
+            </a>
+          </div>
+        </div>
+      </nav>
+
+    {{--<div class="columns navbar mb-6" style="background-color:hsl(0, 0%, 92%);">
+        <div class="column nav is-1" style="background-color:#d53021;">
             <img class="mt-0 pt-0 ml-5"src="topleft.jpg" alt="centered image" style="width:50px; margin-bottom:-10px"/>
         </div>
         <div class="column nav button invertable" style="background-color:#d53021;">
@@ -243,13 +277,13 @@
                 </div>
             </span>
         </div>
-    </div> --}}
+    </div>--}}
 
     @yield('content')
 
     <footer>
-        <div class="columns is-centered pt-4 invertable mb-1">
-            <div class="column is-5 invertable">
+        <div class="columns is-centered pt-4 mb-1">
+            <div class="column is-5">
                 <p>ATLAS is an autism-led social enterprise.</p>
                 <br> 
                 <p>We provide training and advice.</p>
@@ -271,24 +305,21 @@
         $( document ).ready(function() {
             $('body').fadeIn(1500); // fadein time in ms
 
-            $(".defaultScheme").click(function(event){
-                $('.invertable').removeClass('inverted');
-            });
+            // Check for click events on the navbar burger icon
+            $(".navbar-burger").click(function() {
 
-            $(".invertedScheme").click(function(event){
-                $('.invertable').addClass('inverted');
+                // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+                $(".navbar-burger").toggleClass("is-active");
+                $(".navbar-menu").toggleClass("is-active");
+
             });
 
             $(".font-sm").click(function(event){
-                $('.sizeable').removeClass('font-lg font-md').addClass('font-sm');
-            });
-
-            $(".font-md").click(function(event){
-                $('.sizeable').removeClass('font-lg font-sm').addClass('font-md');
+                $('.sizeable').removeClass('font-lg').addClass('font-sm');
             });
 
             $(".font-lg").click(function(event){
-                $('.sizeable').removeClass('font-md font-sm').addClass('font-lg');
+                $('.sizeable').removeClass('font-sm').addClass('font-lg');
             });
         });
     </script>
