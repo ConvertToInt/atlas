edefb5bb8ac0aced8ccceaf60fb91ee37a0533b7
ConvertToInt <tdalgleish7@gmail.com>
2021-09-16 07:36:11 +0100

    meet the team page basic

---
+++ resources/views/team.blade.php
@@ -0,0 +1,89 @@
+@extends('layout')
+
+@section('head')
+
+@endsection
+
+@section('content')
+
+<div class="columns is-centered mt-6 mb-6">
+    <div class="column is-8">
+        <div class="box" style="background:hsl(0, 0%, 95%)">
+            <div class="columns">
+                <div class="column is-2 is-left">
+                    <figure class="image is-128x128">
+                        <img src="https://bulma.io/images/placeholders/128x128.png">
+                      </figure>
+                </div>
+                <div class="column font-sm sizeable">
+                    <h1 class="title has-text-centered has-text-weight-bold has-text-grey-darker is-size-3 pt-3 mt-4">Kira McCarthy</h1>
+                    <h1 class="title has-text-centered has-text-weight-bold has-text-grey-darker is-size-5">Director &middot; Practitioner &middot; Funny</h1>
+                </div>
+            </div>
+        </div>
+        <div class="box" style="background:hsl(0, 0%, 95%)">
+            <div class="columns">
+                <div class="column is-2 is-left">
+                    <figure class="image is-128x128">
+                        <img src="https://bulma.io/images/placeholders/128x128.png">
+                      </figure>
+                </div>
+                <div class="column">
+                    <h1 class="title has-text-centered has-text-weight-bold has-text-grey-darker is-size-3 pt-3 mt-4">Kira McCarthy</h1>
+                    <h1 class="title has-text-centered has-text-weight-bold has-text-grey-darker is-size-5">Director &middot; Practitioner &middot; Funny</h1>
+                </div>
+            </div>
+        </div>
+        <div class="box" style="background:hsl(0, 0%, 95%)">
+            <div class="columns">
+                <div class="column is-2 is-left">
+                    <figure class="image is-128x128">
+                        <img src="https://bulma.io/images/placeholders/128x128.png">
+                      </figure>
+                </div>
+                <div class="column">
+                    <h1 class="title has-text-centered has-text-weight-bold has-text-grey-darker is-size-3 pt-3 mt-4">Kira McCarthy</h1>
+                    <h1 class="title has-text-centered has-text-weight-bold has-text-grey-darker is-size-5">Director &middot; Practitioner &middot; Funny</h1>
+                </div>
+            </div>
+        </div>
+        <div class="box has-background-white-bis">
+            <div class="columns">
+                <div class="column is-2 is-left">
+                    <figure class="image is-128x128">
+                        <img src="https://bulma.io/images/placeholders/128x128.png">
+                      </figure>
+                </div>
+                <div class="column">
+                    <h1 class="title has-text-centered has-text-weight-bold has-text-grey-darker is-size-3 pt-3 mb-6 mt-6">John</h1>
+                </div>
+            </div>
+        </div>
+        <div class="box has-background-white-bis">
+            <div class="columns">
+                <div class="column is-2 is-left">
+                    <figure class="image is-128x128">
+                        <img src="https://bulma.io/images/placeholders/128x128.png">
+                      </figure>
+                </div>
+                <div class="column">
+                    <h1 class="title has-text-centered has-text-weight-bold has-text-grey-darker is-size-3 pt-3 mb-6 mt-6">Sean</h1>
+                </div>
+            </div>
+        </div>
+        <div class="box has-background-white-bis">
+            <div class="columns">
+                <div class="column is-2 is-left">
+                    <figure class="image is-128x128">
+                        <img src="https://bulma.io/images/placeholders/128x128.png">
+                      </figure>
+                </div>
+                <div class="column">
+                    <h1 class="title has-text-centered has-text-weight-bold has-text-grey-darker is-size-3 pt-3 mb-6 mt-6">Amy</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
